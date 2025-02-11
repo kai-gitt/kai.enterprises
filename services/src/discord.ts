17ab6e501ab2d2f83793e4c009f8aaa94ae179d9
Kai <km127pl@cloudnode.pro>
2025-02-11 19:47:05 +0100

    add spotify back

--- services/src/discord.ts
+++ services/src/discord.ts
@@ -10,7 +10,6 @@ export const fetchDiscordApi = async (
 		const data = (await req.json()) as DiscordResponse;
 		// delete the unnecessary data
 		delete data.data.activities;
-		delete data.data.spotify;
 		delete data.data.discord_user;
 		return data;
 	} catch (e) {
