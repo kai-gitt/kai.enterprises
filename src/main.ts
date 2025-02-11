28df01a1974c98bde5c79b742a731e2c27ab04a7
Kai <km127pl@cloudnode.pro>
2025-02-11 01:29:38 +0100

    Add editorconfig

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
 import "./main.css";
+
+const TZ_OFFSET = 1;
+
+let clock: HTMLParagraphElement;
+
+document.addEventListener("DOMContentLoaded", (_) => {
+	clock = document.getElementById("current_time")! as HTMLParagraphElement;
+	setInterval(UpdateClock, 100);
+});
+
+const UpdateClock = () => {
+	let time = new Date(Date.now());
+	clock.innerText = `${((time.getUTCHours() + TZ_OFFSET) % 24)
+		.toString(10)
+		.padStart(2, "0")}:${time.getUTCMinutes()}:${time.getUTCSeconds()}`;
+};
