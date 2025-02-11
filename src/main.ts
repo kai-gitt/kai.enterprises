90474655ade50fb36e8435b65007e0a0cd4dc5ef
Kai <km127pl@cloudnode.pro>
2025-02-11 01:38:07 +0100

    Add a wakatime widget

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,20 @@ import "./main.css";
 const TZ_OFFSET = 1;
 
 let clock: HTMLParagraphElement;
+let wakatime: HTMLParagraphElement;
 
-document.addEventListener("DOMContentLoaded", (_) => {
+interface CumulativeTotal {
+	decimal: string;
+	digital: string;
+	seconds: number;
+	text: string;
+}
+
+document.addEventListener("DOMContentLoaded", async (_) => {
 	clock = document.getElementById("current_time")! as HTMLParagraphElement;
+	wakatime = document.getElementById("wakatime")! as HTMLParagraphElement;
 	setInterval(UpdateClock, 100);
+	await UpdateWakatimeData();
 });
 
 const UpdateClock = () => {
@@ -14,4 +24,10 @@ const UpdateClock = () => {
 	clock.innerText = `${((time.getUTCHours() + TZ_OFFSET) % 24)
 		.toString(10)
 		.padStart(2, "0")}:${time.getUTCMinutes()}:${time.getUTCSeconds()}`;
+};
+
+const UpdateWakatimeData = async (): Promise<void> => {
+	const req = await fetch("https://services.kai.enterprises/wakatime");
+	const data = (await req.json()) as CumulativeTotal;
+	wakatime.innerText = data.text;
 };
