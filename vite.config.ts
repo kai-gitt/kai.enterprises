031d4674edf4c8a03344d114f35185c8fe57d9c5
Kai <me@kai.enterprises>
2025-02-17 00:13:33 +0100

    minify

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	plugins: [tailwindcss()]
+	plugins: [tailwindcss()],
+	build: {
+		minify: "esbuild",
+	},
 });
